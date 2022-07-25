69e4dcc068e974508675cc8a2279888565377c93
Abjasree <83914220+abjasree@users.noreply.github.com>
2022-07-25 16:59:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import pickle
 
 st.write("""
-# maximum number app
-This app predicts the credit card approval probablity
+# Test Score Prediction App
+This app predicts your test score based on number of hours you have studied in a week
 """)
 #Get Input
 
@@ -11,17 +10,14 @@ st.header('User Input Parameters')
 
 def user_input_features():
    
-    number_1 = st.number_input("First Number",min_value=-2000000.0,max_value=2000000.0)
-    number_2 = st.number_input("Second Number",min_value=-2000000.0,max_value=2000000.0)
-    number_3 = st.number_input("Third Number", min_value=2000000.0,max_value=2000000.0)
-    data = {'number_1':number_1, 'number_2':number_2, 'number_3':number_3}
-    return data
+    num = st.number_input("Hour's studied in a week",min_value= 0.0,max_value= 168.0)
+   
+    
+    return num
 
-d = user_input_features()
-st.subheader('Largest Number')
-if d['number_2'] <= d['number_1'] and d['number_3'] <= d['number_1']:
-    st.write(d['number_1'])
-elif d['number_1'] <= d['number_2'] and d['number_3'] <= d['number_2']:
-    st.write(d['number_2'])
-else:
-    st.write(d['number_3'])
+x = user_input_features()
+b = 0.02963934787473239
+m = 1.4774173755483797
+y = m*x + b
+st.subheader('The test score you can expect considering the hours you studied:')
+st.write(y)
