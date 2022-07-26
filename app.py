b737595d3223900e09d84232a34ab7da8957d63d
Abjasree <83914220+abjasree@users.noreply.github.com>
2022-07-26 14:31:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,9 @@ m = 1.4774173755483797
 y = round(m*x + b, 2)
 st.subheader('The test score you can expect considering the hours you studied:')
 if x:
-   st.write(y)
+   if y < 100.0:
+      st.write(y)
+   else:
+      st.write(100.0)
 else:
    st.write(0.0)
